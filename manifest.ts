fd8bb2927f9ac8e87d7284d4d510948519268bfc
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:08:16 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Books Library',
-  icon: 'tabler:books',
   provider: lazy(() => import('@/providers/BooksLibraryProvider')),
   routes: {
     '/': lazy(() => import('@'))
-  },
-  category: 'Storage'
+  }
 } satisfies ModuleConfig
